f2c2ffff1e720ae12ab8e434e17a6eed18aa7a39
Valerian Saliou <valerian@valeriansaliou.name>
2018-01-10 19:26:47 +0100

    Initialize
    Signed-off-by: Valerian Saliou <valerian@valeriansaliou.name>

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+// Vigil
+//
+// Microservices Status Page
+// Copyright: 2018, Valerian Saliou <valerian@valeriansaliou.name>
+// License: Mozilla Public License v2.0 (MPL v2.0)
+
+// #[macro_use]
+// extern crate log;
+
+fn main() {
+    // TODO
+}
