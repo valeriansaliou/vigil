626a98583408bcda33f0bdffb4190ab113fc904c
Valerian Saliou <valerian@valeriansaliou.name>
2018-01-15 19:46:04 +0100

    Config OK
    Signed-off-by: Valerian Saliou <valerian@valeriansaliou.name>

--- src/responder/manager.rs
+++ src/responder/manager.rs
@@ -5,13 +5,21 @@
 // License: Mozilla Public License v2.0 (MPL v2.0)
 
 use rocket;
+use rocket::config::{Config, Environment};
 
 use super::routes;
 
-pub fn run() {
-    // TODO: pass listen in config.cfg configuration, do not read Rocket.toml
+use APP_CONF;
 
-    rocket::ignite()
+pub fn run() {
+    let config = Config::build(Environment::Production)
+        .address(APP_CONF.server.inet.ip().to_string())
+        .port(APP_CONF.server.inet.port())
+        .workers(2)
+        .finalize()
+        .unwrap();
+
+    rocket::custom(config, false)
         .mount("/", routes![
             routes::index,
             routes::badge
