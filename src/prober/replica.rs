4857c0f1cfdc48c9c9d9665eec805491eae43a9a
Valerian Saliou <valerian@valeriansaliou.name>
2020-01-14 11:34:09 +0100

    Fix URL parsing for IPv6 URLs for TCP & ICMP protocols + Stricter URL parsing
    Signed-off-by: Valerian Saliou <valerian@valeriansaliou.name>

--- src/prober/replica.rs
+++ src/prober/replica.rs
@@ -4,7 +4,7 @@
 // Copyright: 2018, Valerian Saliou <valerian@valeriansaliou.name>
 // License: Mozilla Public License v2.0 (MPL v2.0)
 
-use url::Url;
+use url::{Host, Url};
 
 #[derive(Serialize, Debug, Clone)]
 pub enum ReplicaURL {
@@ -17,20 +17,35 @@ pub enum ReplicaURL {
 impl ReplicaURL {
     pub fn parse_from(raw_url: &str) -> Result<ReplicaURL, ()> {
         match Url::parse(raw_url) {
-            Ok(parsed_url) => match parsed_url.scheme() {
-                "icmp" => match parsed_url.host_str() {
-                    Some(host) => Ok(ReplicaURL::ICMP(host.to_string())),
+            Ok(url) => match url.scheme() {
+                "icmp" => match (url.host(), url.port(), url.path_segments()) {
+                    (Some(host), None, None) => Ok(ReplicaURL::ICMP(Self::host_string(host))),
                     _ => Err(()),
                 },
-                "tcp" => match (parsed_url.host_str(), parsed_url.port()) {
-                    (Some(host), Some(port)) => Ok(ReplicaURL::TCP(host.to_string(), port)),
+                "tcp" => match (url.host(), url.port(), url.path_segments()) {
+                    (Some(host), Some(port), None) => {
+                        Ok(ReplicaURL::TCP(Self::host_string(host), port))
+                    }
                     _ => Err(()),
                 },
-                "http" => Ok(ReplicaURL::HTTP(parsed_url.into_string())),
-                "https" => Ok(ReplicaURL::HTTPS(parsed_url.into_string())),
+                "http" => Ok(ReplicaURL::HTTP(url.into_string())),
+                "https" => Ok(ReplicaURL::HTTPS(url.into_string())),
                 _ => Err(()),
             },
             _ => Err(()),
+        }
+    }
+
+    fn host_string(host: Host<&str>) -> String {
+        // Convert internal host value into string. This is especially useful for IPv6 addresses, \
+        //   which we need returned in '::1' format; as they would otherwise be returned in \
+        //   '[::1]' format using built-in top-level 'to_string()' method on the 'Host' trait. The \
+        //   underlying address parser does not accept IPv6 addresses formatted as '[::1]', so \
+        //   this seemingly overkill processing is obviously needed.
+        match host {
+            Host::Domain(domain_value) => domain_value.to_string(),
+            Host::Ipv4(ipv4_value) => ipv4_value.to_string(),
+            Host::Ipv6(ipv6_value) => ipv6_value.to_string(),
         }
     }
 }
