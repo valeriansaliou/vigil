668378ae4cc056e590d7c246d2ef072304241c7e
Valerian Saliou <valerian@valeriansaliou.name>
2020-01-20 10:20:12 +0100

    Fix
    Signed-off-by: Valerian Saliou <valerian@valeriansaliou.name>

--- scripts/release/release_binaries.sh
+++ scripts/release/release_binaries.sh
@@ -38,8 +38,8 @@ function release_for_architecture {
     final_tar="v$VIGIL_VERSION-$1.tar.gz"
 
     rm -rf ./vigil/ && \
-        docker run --rm -it -v "$(pwd)":/home/rust/src ekidd/rust-musl-builder:stable cargo build --target=$2 --release && \
-        docker run --rm -it -v "$(pwd)":/home/rust/src ekidd/rust-musl-builder:stable strip ./target/$2/release/vigil && \
+        docker run --rm -it -v "$(pwd)":/home/rust/src ekidd/rust-musl-builder:nightly cargo build --target=$2 --release && \
+        docker run --rm -it -v "$(pwd)":/home/rust/src ekidd/rust-musl-builder:nightly strip ./target/$2/release/vigil && \
         mkdir ./vigil && \
         mv "target/$2/release/vigil" ./vigil/ && \
         cp -r ./config.cfg ./res vigil/ && \
