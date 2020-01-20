dffff5efacf5242661b6edef4fef0101af25a0c4
Valerian Saliou <valerian@valeriansaliou.name>
2020-01-20 10:18:32 +0100

    Move packaging to a single easy script (multi-arch release)
    Signed-off-by: Valerian Saliou <valerian@valeriansaliou.name>

--- PACKAGING.md
+++ PACKAGING.md
@@ -3,9 +3,9 @@ Packaging
 
 This file contains quick reminders and notes on how to package Vigil.
 
-We consider here the packaging flow of Vigil version `1.0.0` for Linux, for target architecture `x86_64` (the steps are alike for `i686`):
+We consider here the packaging flow of Vigil version `1.0.0` for Linux.
 
-1. **How to setup `rust-musl-builder` on MacOS:**
+1. **How to setup `rust-musl-builder` on MacOS (required to build binaries):**
     1. Follow setup instructions from: [rust-musl-builder](https://github.com/emk/rust-musl-builder)
     2. Pull the nightly Docker image: `docker pull ekidd/rust-musl-builder:nightly`
 
@@ -13,20 +13,16 @@ We consider here the packaging flow of Vigil version `1.0.0` for Linux, for targ
     1. Bump version in `Cargo.toml` to `1.0.0`
     2. Execute `cargo update` to bump `Cargo.lock`
 
-3. **How to build Vigil for Linux on MacOS:**
+3. **How to build Vigil for Linux on MacOS (multiple architectures):**
     1. `rust-musl-builder-nightly cargo build --target=x86_64-unknown-linux-musl --release`
     2. `rust-musl-builder-nightly strip ./target/x86_64-unknown-linux-musl/release/vigil`
 
-4. **How to package built binary and release it on GitHub:**
-    1. `mkdir vigil`
-    2. `mv target/x86_64-unknown-linux-musl/release/vigil vigil/`
-    4. `cp -r config.cfg res vigil/`
-    5. `tar -czvf v1.0.0-x86_64.tar.gz vigil`
-    6. `rm -r vigil/`
-    7. Publish the archive on the [releases](https://github.com/valeriansaliou/vigil/releases) page on GitHub
+4. **How to build Vigil, package it and release it on GitHub:**
+    1. `./scripts/release/release_binaries.sh --version=1.0.0`
+    2. Publish the built archives on the [releases](https://github.com/valeriansaliou/vigil/releases) page on GitHub
 
 5. **How to update Crates:**
-    1. Publish package on Crates: `cargo publish`
+    1. Publish package on Crates: `cargo publish --no-verify`
 
 6. **How to update Docker:**
     1. `docker build .`
---
+++ scripts/release/release_binaries.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+##
+#  Vigil
+#
+#  Microservices Status Page
+#  Copyright: 2020, Valerian Saliou <valerian@valeriansaliou.name>
+#  License: Mozilla Public License v2.0 (MPL v2.0)
+##
+
+# Read arguments
+while [ "$1" != "" ]; do
+    argument_key=`echo $1 | awk -F= '{print $1}'`
+    argument_value=`echo $1 | awk -F= '{print $2}'`
+
+    case $argument_key in
+        -v | --version)
+            VIGIL_VERSION="$argument_value"
+            ;;
+        *)
+            echo "Unknown argument received: '$argument_key'"
+            exit 1
+            ;;
+    esac
+
+    shift
+done
+
+# Ensure release version is provided
+if [ -z "$VIGIL_VERSION" ]; then
+  echo "No Vigil release version was provided, please provide it using '--version'"
+
+  exit 1
+fi
+
+# Define release pipeline
+function release_for_architecture {
+    final_tar="v$VIGIL_VERSION-$1.tar.gz"
+
+    rm -rf ./vigil/ && \
+        docker run --rm -it -v "$(pwd)":/home/rust/src ekidd/rust-musl-builder:stable cargo build --target=$2 --release && \
+        docker run --rm -it -v "$(pwd)":/home/rust/src ekidd/rust-musl-builder:stable strip ./target/$2/release/vigil && \
+        mkdir ./vigil && \
+        mv "target/$2/release/vigil" ./vigil/ && \
+        cp -r ./config.cfg ./res vigil/ && \
+        tar -czvf "$final_tar" ./vigil && \
+        rm -r ./vigil/
+    release_result=$?
+
+    if [ $release_result -eq 0 ]; then
+        echo "Result: Packed architecture: $1 to file: $final_tar"
+    fi
+
+    return $release_result
+}
+
+# Run release tasks
+ABSPATH=$(cd "$(dirname "$0")"; pwd)
+BASE_DIR="$ABSPATH/../../"
+
+rc=0
+
+pushd "$BASE_DIR" > /dev/null
+    echo "Executing release steps for Vigil v$VIGIL_VERSION..."
+
+    release_for_architecture "x86_64" "x86_64-unknown-linux-musl" && \
+        release_for_architecture "armv7" "armv7-unknown-linux-musleabihf"
+    rc=$?
+
+    if [ $rc -eq 0 ]; then
+        echo "Success: Done executing release steps for Vigil v$VIGIL_VERSION"
+    else
+        echo "Error: Failed executing release steps for Vigil v$VIGIL_VERSION"
+    fi
+popd > /dev/null
+
+exit $rc
