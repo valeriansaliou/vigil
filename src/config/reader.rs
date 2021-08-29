b30fe40e06c0bbc1d7a155bdbfc3b9f84ca032f3
Valerian Saliou <valerian@valeriansaliou.name>
2021-08-29 10:46:56 +0200

    Fix #96

--- src/config/reader.rs
+++ src/config/reader.rs
@@ -4,6 +4,7 @@
 // Copyright: 2018, Valerian Saliou <valerian@valeriansaliou.name>
 // License: Mozilla Public License v2.0 (MPL v2.0)
 
+use std::collections::hash_set::HashSet;
 use std::fs::File;
 use std::io::Read;
 
@@ -27,6 +28,45 @@ impl ConfigReader {
 
         debug!("read config file: {}", &APP_ARGS.config);
 
-        toml::from_str(&conf).expect("syntax error in config file")
+        // Parse configuration
+        let config = toml::from_str(&conf).expect("syntax error in config file");
+
+        // Validate configuration
+        Self::validate(&config);
+
+        config
+    }
+
+    fn validate(config: &Config) {
+        // Validate all identifiers
+        Self::validate_identifiers(config)
+    }
+
+    fn validate_identifiers(config: &Config) {
+        // Scan for service identifier duplicates
+        let mut service_identifiers = HashSet::new();
+
+        for service in config.probe.service.iter() {
+            // Service identifier was already previously inserted? (caught a duplicate)
+            if service_identifiers.insert(&service.id) == false {
+                panic!(
+                    "configuration has duplicate service identifier: {}",
+                    service.id
+                )
+            }
+
+            // Scan for node identifier duplicates
+            let mut node_identifiers = HashSet::new();
+
+            for node in service.node.iter() {
+                // Node identifier was already previously inserted? (caught a duplicate)
+                if node_identifiers.insert(&node.id) == false {
+                    panic!(
+                        "configuration has duplicate node identifier: {} in service: {}",
+                        node.id, service.id
+                    )
+                }
+            }
+        }
     }
 }
